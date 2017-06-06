86df2521cbd18aa6c141e4115d3afe35b40afa89
aishwarya-ksr <aishwarya04111996@gmail.com>
2017-06-05 22:09:26 -0700

    Create revint.java

---
+++ revint.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util*;
+class revint
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int m,sum=0;
+while(n!=0)
+{
+m=n%10;
+n=n/10;
+System.out.println(m);
+}
+}
+}
