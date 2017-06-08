a93e81f517aad044bfdbf511f58e97632142bdaa
aishwarya-ksr <aishwarya04111996@gmail.com>
2017-06-08 06:16:07 -0700

    Create wrev.java

---
+++ wrev.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class wrev
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+String a[]=s.split(" ");
+for(int i=a.length-1;i>=0;i--)
+{
+System.out.print(a[i]+" ");
+}
+}
+}
