8453e1741ceef2cd099c6c53ff3f137f460ab58b
aishwarya-ksr <aishwarya04111996@gmail.com>
2017-06-12 15:00:41 +0530

    Create duplicate.java

---
+++ duplicate.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+class duplicate
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+String ans="";
+char c;
+int j,i=0;
+j=s.length();
+s+=" ";
+c=s.charAt(0);
+while(i<j)
+{
+ans=ans+c;
+while(s.charAt(++i)==c&&i<j)
+{}
+c=s.charAt(i);
+}
+System.out.println(ans);
+}
+}
