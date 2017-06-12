af03aa62396fb18063214cacd061089ef72140bb
aishwarya-ksr <aishwarya04111996@gmail.com>
2017-06-12 15:12:19 +0530

    Create vowel.java

--- vowel.java
+++ vowel.java
@@ -4,7 +4,7 @@ class vowel
 {
 public static void main(String args[])
 {
-Scanner sc=nextInt();
+Scanner sc=new Scanner(System.in);
 char ch=sc.next().charAt(0);
 if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
 {
