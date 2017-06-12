ebee1d3edcded81a39300046d2ae70a059cd6c8d
aishwarya-ksr <aishwarya04111996@gmail.com>
2017-06-12 17:20:48 +0530

    Create sumnumber.java

---
+++ sumnumber.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class sumnumber
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int number=sc.nextInt();
+int s=0;
+  int i;
+for(j=0;j<=number;i++)
+{
+s=s+i;
+}
+System.out.println(s);
+}
+}
