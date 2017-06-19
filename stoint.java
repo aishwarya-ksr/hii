247015aaf4f162a71849b6056bc1a5800e1450bd
aishwarya-ksr <aishwarya04111996@gmail.com>
2017-06-19 03:04:40 -0700

    Create stoint.java

---
+++ stoint.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class stoint
+{
+public static void main(String args[])
+{
+
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+int n=Integer.parseInt(s);
+System.out.println(n);
+}
+}
